71eb37c88be01ffe3b5cde7e1d66b1986838dc34
Yichen Yang <yichen.yang@flatironschool.com>
2016-05-30 17:45:45 -0400

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,14 +3,12 @@ def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
+status = "rescued"
 
 
 
 ## Comment the below back in to see method scope in action!
-
+rescue_princess_peach
+puts status
 # rescue_princess_peach
 # puts status
-
-
-
-
